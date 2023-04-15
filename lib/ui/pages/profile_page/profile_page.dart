d4dcb2cf5f497c3b3fb333dea3bbb71f4428eaaf
Arshad Malik <arshadmallik47@gmail.com>
2023-04-15 08:50:32 +0500

    profileview setup

--- lib/ui/pages/profile_page/profile_page.dart
+++ lib/ui/pages/profile_page/profile_page.dart
@@ -1,12 +1,20 @@
+import 'package:bgtv/ui/pages/profile_page/views/dashboard_view.dart';
+import 'package:bgtv/ui/pages/profile_page/views/profile_view.dart';
+import 'package:bgtv/ui/pages/profile_page/views/subscription_view.dart';
+import 'package:bgtv/ui/pages/profile_page/views/support_view.dart';
 import 'package:bgtv/ui/pages/profile_page/widgets/view_selector%20_widget.dart';
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../../core/profile_provider.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final profileProvider = Provider.of<ProfileProvider>(context);
     return SingleChildScrollView(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -101,169 +109,70 @@ class ProfilePage extends StatelessWidget {
             ),
           ),
           Container(
-            
-            padding: const EdgeInsets.only(left: 10),
+            padding: const EdgeInsets.only(left: 10, top: 10,bottom: 10),
             margin: const EdgeInsets.symmetric(horizontal: 30),
             height: 80,
             color: Colors.black54,
-            child: Column(
-              children: [
-                Row(
-                  
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    ViewSelectorWidget(
-                      onTap: () {},
-                      title: 'Dashboard',
-                      icon: Icons.home,
-                      // height: 40,
-                    ),
-                    ViewSelectorWidget(
-                      onTap: () {},
-                      title: 'Profile details',
-                      icon: Icons.person,
-                      // height: 40,
-                    )
-                  ],
-                ),
-                Row(
-                  children: [
-                    ViewSelectorWidget(
-                      onTap: () {},
-                      title: 'Subscriptions',
-                      icon: Icons.notifications,
-                      // height: 45,
-                    ),
-                    ViewSelectorWidget(
-                      onTap: () {},
-                      title: ' support',
-                      icon: Icons.help_outline,
-                      //height: 45,
-                    ),
-                  ],
-                ),
-              ],
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      ViewSelectorWidget(
+                        onTap: () {
+                          profileProvider.updateView(ProfileView.dashboard);
+                        },
+                        selected: profileProvider.selectedView == ProfileView.dashboard,
+                        title: 'Dashboard',
+                        icon: Icons.home,
+                        // height: 40,
+                      ),
+                      ViewSelectorWidget(
+                        onTap: () {
+                          profileProvider.updateView(ProfileView.profile);
+                        },
+                        selected: profileProvider.selectedView == ProfileView.profile,
+                        title: 'Profile details',
+                        icon: Icons.person,
+                        // height: 40,
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 10,),
+                  Row(
+                    children: [
+                      ViewSelectorWidget(
+                        onTap: () {
+                          profileProvider.updateView(ProfileView.subscriptions);
+                        },
+                        selected: profileProvider.selectedView == ProfileView.subscriptions,
+                        title: 'Subscriptions',
+                        icon: Icons.notifications,
+                        // height: 45,
+                      ),
+                      ViewSelectorWidget(
+                        onTap: () {
+                          profileProvider.updateView(ProfileView.support);
+                        },
+                        selected: profileProvider.selectedView == ProfileView.support,
+                        title: ' support',
+                        icon: Icons.help_outline,
+                        //height: 45,
+                      ),
+                    ],
+                  ),
+
+                ],
+              ),
             ),
           ),
-          Container(
-            padding: const EdgeInsets.only(left: 35, top: 5, right: 60, bottom: 10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'Edit Your account',
-                  style: TextStyle(
-                      color: Colors.black, fontWeight: FontWeight.w500),
-                ),
-                const SizedBox(
-                  height: 8,
-                ),
-
-                const Text(
-                  '*Email',
-                  style: TextStyle(
-                      color: Colors.black, fontWeight: FontWeight.w500),
-                ),
-                SizedBox(
-                  height: 35,
-                  width: double.infinity,
-                  child: TextFormField(
-                    readOnly: false,
-                    decoration: const InputDecoration(border: OutlineInputBorder()),
-                  ),
-                ),
-                const Text(
-                  '*First Name',
-                  style: TextStyle(
-                      color: Colors.black, fontWeight: FontWeight.w500),
-                ),
-
-                SizedBox(
-                  height: 35,
-                  width: double.infinity,
-                  child: TextFormField(
-                    readOnly: false,
-                    decoration: const InputDecoration(border: OutlineInputBorder()),
-                  ),
-                ),
-
-                const Text(
-                  '*Last Name',
-                  style: TextStyle(
-                      color: Colors.black, fontWeight: FontWeight.w500),
-                ),
-                SizedBox(
-                  height: 35,
-                  width: double.infinity,
-                  child: TextFormField(
-                    readOnly: false,
-                    decoration: const InputDecoration(border: OutlineInputBorder()),
-                  ),
-                ),
-
-                const Text(
-                  '*Phone',
-                  style: TextStyle(
-                      color: Colors.black, fontWeight: FontWeight.w500),
-                ),
-                SizedBox(
-                  height: 35,
-                  width: double.infinity,
-                  child: TextFormField(
-                    decoration: const InputDecoration(border: OutlineInputBorder()),
-                  ),
-                ),
-
-                 const Text(
-                  '*Password',
-                  style: TextStyle(
-                      color: Colors.black, fontWeight: FontWeight.w500),
-                ),
-                SizedBox(
-                  height: 35,
-                  width: double.infinity,
-                  child: TextFormField(
-                    decoration: const InputDecoration(border: OutlineInputBorder()),
-                  ),
-                ),
-
-                 const Text(
-                  '*Confirm Password',
-                  style: TextStyle(
-                      color: Colors.black, fontWeight: FontWeight.w500),
-                ),
-                SizedBox(
-                  height: 35,
-                  width: double.infinity,
-                  child: TextFormField(
-                    decoration: const InputDecoration(border: OutlineInputBorder()),
-                  ),
-                ),
-
-              ],
-            ),
-          ),
-           Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-             children: [
-               Container(
-                
-                alignment: Alignment.center,
-                margin: const EdgeInsets.only(right: 60, top: 10, bottom: 20),
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                height: 30,
-                width: 120,
-                child: const Text(
-                  'SAVE CHANGES',
-                  style: TextStyle(color: Colors.white),
-                ),
-          ),
-             ],
-           ),
-
+           if (profileProvider.selectedView == ProfileView.dashboard) const DashboardView(),
+                  if (profileProvider.selectedView == ProfileView.profile) const ProfilePageView(),
+                  if (profileProvider.selectedView == ProfileView.subscriptions) const SubscriptionView(),
+                  if (profileProvider.selectedView == ProfileView.support) const SupportView(),
+    
+      
         ],
       ),
     );
