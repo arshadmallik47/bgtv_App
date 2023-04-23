dbd36e28d97abfd9284492412b7f942c84776eb6
Arshad Malik <arshadmallik47@gmail.com>
2023-04-23 17:57:39 +0500

    page setup

---
+++ lib/ui/main_screen/pages/feed_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+class FeedPage extends StatelessWidget {
+  const FeedPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Search Page', style: TextStyle(fontSize: 40),
+        ),
+      ),
+    );
+  }
+}
